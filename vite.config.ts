440f822b96336f0ba2d3f78f32c38b3b5b11d450
Priyanshu <priyanshu@dev.com>
2026-05-16 02:23:26 +0530

    chore: Initial project setup with TypeScript, Vite, ESLint, and Tailwind CSS configuration

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        }
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        background: resolve(__dirname, 'src/background/index.ts'),
+        content: resolve(__dirname, 'src/content/index.ts'),
+      },
+      output: {
+        entryFileNames: '[name]/index.js',
+        chunkFileNames: 'shared/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
