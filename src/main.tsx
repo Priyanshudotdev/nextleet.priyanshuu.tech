7bc9b5a5ac10d72079cacc4e26c550e24721f138
Priyanshu <priyanshu@dev.com>
2026-05-17 15:51:17 +0530

    feat: add client-side routing with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
 import './global.css'
-import { Popup } from './popup/popup.tsx'
+import { Home } from './popup/home.tsx'
+import LoginSection from './popup/login.tsx'
+import RepoSetupSection from './popup/repo-setup.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="bg-white w-[600px] h-[500px]">
-      <Popup />
+    <div className="relative w-90 min-h-120 bg-blue-600">
+      <HashRouter>
+      <Routes>
+        <Route path='/index.html' element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Home />} />
+        
+        <Route path='/login' element={<LoginSection />} />
+        <Route path='/repo-setup' element={<RepoSetupSection />} />
+      </Routes>
+      </HashRouter>
     </div>
   </StrictMode>,
 )
